72f0086d769d680ff6e63d2231c81123c606dc8d
vhreptovich <vhreptovich@gmail.com>
2024-03-20 09:09:55 +0300

    Add init

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,16 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { StudentType } from '../student/student.type';
+
+@ObjectType('Lesson')
+export class LessonType {
+  @Field(() => ID)
+  id: string;
+  @Field()
+  name: string;
+  @Field()
+  startDate: string;
+  @Field()
+  endDate: string;
+  @Field(() => [StudentType])
+  students: string[];
+}
---
+++ src/student/student.type.ts
@@ -0,0 +1,11 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType('Student')
+export class StudentType {
+  @Field(() => ID)
+  id: string;
+  @Field()
+  firstName: string;
+  @Field()
+  lastName: string;
+}
